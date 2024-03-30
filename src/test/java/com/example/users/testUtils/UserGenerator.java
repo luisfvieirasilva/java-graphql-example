e9ca0712a4a0656e6a273540c8cf72e4d19ada00
Luis Silva <luisfvieirasilva@gmail.com>
2024-03-30 15:14:11 -0300

    chore: add generateApolloCreateUserInput to UserGenerator

--- src/test/java/com/example/users/testUtils/UserGenerator.java
+++ src/test/java/com/example/users/testUtils/UserGenerator.java
@@ -1,16 +1,29 @@
 package com.example.users.testUtils;
 
+import com.example.test.type.CreateUserInput;
 import com.example.users.codegen.types.User;
 
 public class UserGenerator {
 
     private static int userCounter = 0;
 
-    public static User generateFakeUser() {
-        userCounter++;
+    public static User generateDTOUser() {
+        var id = getNextUserId();
         return User.newBuilder()
-                .id(Integer.toString(userCounter))
-                .name("User" + userCounter)
-                .email("user" + userCounter + "@email.com").build();
+                .id(id)
+                .name("User" + id)
+                .email("user" + id + "@email.com").build();
+    }
+
+    public static CreateUserInput generateApolloCreateUserInput() {
+        var id = getNextUserId();
+        return CreateUserInput.builder()
+                .name("User" + id)
+                .email("user" + id + "@email.com").build();
+    }
+
+    private static String getNextUserId() {
+        userCounter++;
+        return Integer.toString(userCounter);
     }
 }
